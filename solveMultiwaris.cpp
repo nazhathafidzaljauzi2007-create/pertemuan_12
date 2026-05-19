c2fe37d5e8c59c9a1ed3d66d9e2322474491881b
nazhat <nazhathafidzaljauzi2007@gmail.com>
2026-05-19 15:43:41 +0700

    class budi

--- solveMultiwaris.cpp
+++ solveMultiwaris.cpp
@@ -31,3 +31,15 @@ public:
         cout << "pelajar dibuat\n" << endl;
     }
 };
+
+class budi : public pekerja, public pelajar
+{
+public:
+    budi(int pUmur) :
+        orang(pUmur), // hal ini dapat dilakukan jika menggunakan virtual
+        pekerja(pUmur),
+        pelajar(pUmur)
+    {
+        cout << "budi dibuat\n" << endl;
+    }
+};
