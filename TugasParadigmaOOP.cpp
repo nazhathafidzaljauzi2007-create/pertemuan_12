92529fcaf0fd810f996fc4c28ed27f8a18004f16
nazhat <nazhathafidzaljauzi2007@gmail.com>
2026-05-19 16:29:06 +0700

    class rekening bank

--- TugasParadigmaOOP.cpp
+++ TugasParadigmaOOP.cpp
@@ -1,3 +1,23 @@
 #include <iostream>
 using namespace std;
 
+class rekeningBank
+{
+private:
+    string namaPemilik;
+    int saldo;
+public:
+    rekeningBank(string pNama, int pSaldo) :
+        namaPemilik(pNama),
+        saldo(pSaldo)
+    {
+        cout << "Rekening bank dibuat " << namaPemilik << " dan saldo " << saldo << "\n" << endl;
+    }
+
+    virtual void potonganAdmin() = 0;
+
+    void tampilInfo()
+    {
+        cout << "Pemilik " << namaPemilik << " adalah " << saldo << "\n" << endl;
+    }
+};
