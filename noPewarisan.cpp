bcf52cd953046303d267e06d447a91cb9e7ba983
nazhat <nazhathafidzaljauzi2007@gmail.com>
2026-05-19 15:20:34 +0700

    class derived class

--- noPewarisan.cpp
+++ noPewarisan.cpp
@@ -18,3 +18,12 @@ public:
         cout << "Hallo saya Function dari base class" << endl;
     }
 };
+
+class derivedClass : public baseClass1
+{
+public:
+    void perkenalan()
+    {
+        cout << "Hallo saya Function dari derived class" << endl;
+    }
+};
